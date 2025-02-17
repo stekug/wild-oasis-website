6f8edcf638e36ab83e851e1cf1a896e08cb61db6
Stephan Kugler <150922232+stekug@users.noreply.github.com>
2025-02-17 15:33:27 +0100

    feat: Navigation and Main Content (#5)
    - edit Navigation Component
    - edit Header Component
    - edit body and main content position

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ const josefin = Josefin_Sans({
 });
 
 import '@/app/_styles/globals.css';
+import Header from './_components/Header';
 
 export const metadata = {
   title: {
@@ -23,14 +24,13 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
