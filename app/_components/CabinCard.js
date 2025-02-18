8726f52bc5d33eec6a8e23e85c6b148eee83d804
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-18 14:15:25 +0100

    fetch cabins

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,4 +1,4 @@
-import { UsersIcon } from "@heroicons/react/24/solid";
+import { UsersIcon } from '@heroicons/react/24/solid';
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
