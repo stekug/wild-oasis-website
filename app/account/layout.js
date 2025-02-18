10db8726ec933f76636c22a50cdf87b96ab275af
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-18 13:19:51 +0100

    add layout for account area

---
+++ app/account/layout.js
@@ -0,0 +1,8 @@
+export default function AccountLayout({ children }) {
+  return (
+    <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+      <div>Navigation</div>
+      <div className="py-1">{children}</div>
+    </div>
+  );
+}
