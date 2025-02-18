b46322c9d4161131ebc96707009bc5318a024a73
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-18 13:28:46 +0100

    add SideNavigation component

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,7 +1,9 @@
+import SideNavigation from '@/app/_components/SideNavigation';
+
 export default function AccountLayout({ children }) {
   return (
     <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
-      <div>Navigation</div>
+      <SideNavigation />
       <div className="py-1">{children}</div>
     </div>
   );
