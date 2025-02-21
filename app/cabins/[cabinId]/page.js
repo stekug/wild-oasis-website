4e46bb11da64bf41110fba6b6e3362936b19f3c4
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-21 13:35:27 +0100

    add DateSelector and ReservationForm Component

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import { getCabin, getCabins } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import TextExpander from '@/app/_components/TextExpander';
+import DateSelector from '@/app/_components/DateSelector';
+import ReservationForm from '@/app/_components/ReservationForm';
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
@@ -70,8 +72,12 @@ export default async function Page({ params }) {
 
       <div>
         <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
+          Reserve {name} today. Pay on arrival.
         </h2>
+        <div>
+          <DateSelector />
+          <ReservationForm />
+        </div>
       </div>
     </div>
   );
