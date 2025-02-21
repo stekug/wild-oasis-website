3264569551cefaedb59449e22fb65bde0ee9f2d5
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-21 10:10:44 +0100

    add handleFilter function with usePathname, useRouter, useSearchParams

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,7 +1,17 @@
 'use client';
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
 export default function Filter() {
-  function handleFilter(filter) {}
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function handleFilter(filter) {
+    const params = new URLSearchParams(searchParams);
+    params.set('capacity', filter);
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+  }
 
   return (
     <div className="border border-primary-800 flex">
