d3c8d17150ea2d1e5281a5ca1876e6b9a3337a82
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-24 11:49:59 +0100

    add ReservationContext

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -20,7 +20,6 @@ function DateSelector({ settings, cabin, bookedDates }) {
   const discount = 23;
   const numNights = 23;
   const cabinPrice = 23;
-  const range = { from: null, to: null };
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
@@ -30,6 +29,8 @@ function DateSelector({ settings, cabin, bookedDates }) {
       <DayPicker
         className="pt-12 place-self-center"
         mode="range"
+        onSelect={setRange}
+        selected={range}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
