2c81d972d954f3879595bc83f307811bc9ffed63
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-26 12:46:22 +0100

    display google user avatar

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
+import { auth } from '../_lib/auth';
+import Image from 'next/image';
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +25,29 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center gap-4"
+            >
+              <Image
+                className="h-8 rounded-full"
+                src={session.user.image}
+                alt={session.user.name}
+                width={32}
+                height={32}
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
